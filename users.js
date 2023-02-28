68eed5cce43fdc4de9ef3041fe7df41fd52d8163
Ankit Mandloi <87657684+Ankitmandloi23@users.noreply.github.com>
2023-02-28 18:56:42 +0530

    Update users.js

--- users.js
+++ users.js
@@ -2,8 +2,8 @@ import bcrypt from "bcryptjs"
 
 export const users =[
     {
-        name: "Debjit Pramanick",
-        email: "debjit1@gmail.com",
+        name: "Ankitmandloi",
+        email: "ankitmandloi23@gmail.com",
         password: bcrypt.hashSync('1234',8),
         isAdmin: true,
     },
